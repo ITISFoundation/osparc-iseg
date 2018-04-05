241fa5db0129e5104159db733bb44f639f565cdb
Bryn Lloyd <lloyd@itis.ethz.ch>
2018-04-05 16:39:01 +0200

    pass const char* for filenames/comments, etc

--- Core/MatlabExport.h
+++ Core/MatlabExport.h
@@ -15,19 +15,19 @@
 namespace iseg { namespace matexport {
 
 iSegCore_API bool print_mat(const char* filename, float* matrix, int nx, int ny,
-							int nz, char* comment, int commentlength,
-							char* varname, int varnamelength,
+							int nz, const char* comment, int commentlength,
+							const char* varname, int varnamelength,
 							bool complex = false);
 iSegCore_API bool print_matslices(const char* filename, float** matrix, int nx,
-								  int ny, int nz, char* comment,
-								  int commentlength, char* varname,
+								  int ny, int nz, const char* comment,
+								  int commentlength, const char* varname,
 								  int varnamelength, bool complex = false);
 iSegCore_API bool print_mat(const char* filename, unsigned char* matrix, int nx,
-							int ny, int nz, char* comment, int commentlength,
-							char* varname, int varnamelength);
+							int ny, int nz, const char* comment, int commentlength,
+							const char* varname, int varnamelength);
 iSegCore_API bool print_matslices(const char* filename, unsigned char** matrix,
-								  int nx, int ny, int nz, char* comment,
-								  int commentlength, char* varname,
+								  int nx, int ny, int nz, const char* comment,
+								  int commentlength, const char* varname,
 								  int varnamelength);
 
 #ifdef TISSUES_SIZE_TYPEDEF
@@ -37,8 +37,8 @@ iSegCore_API bool print_mat(const char* filename, tissues_size_t* matrix,
 							int commentlength, char* varname,
 							int varnamelength);
 iSegCore_API bool print_matslices(const char* filename, tissues_size_t** matrix,
-								  int nx, int ny, int nz, char* comment,
-								  int commentlength, char* varname,
+								  int nx, int ny, int nz, const char* comment,
+								  int commentlength, const char* varname,
 								  int varnamelength);
 
 #endif // TISSUES_SIZE_TYPEDEF
