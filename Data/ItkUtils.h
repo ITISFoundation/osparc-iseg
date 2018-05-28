e6bd8e6cd633f143adfca1262763d3d08fa2887b
Bryn Lloyd <lloyd@itis.ethz.ch>
2018-05-28 16:15:39 +0200

    moved MakeBall to ItkUtils added another version of Paste function

--- Data/ItkUtils.h
+++ Data/ItkUtils.h
@@ -12,8 +12,9 @@
 #include "itkSliceContiguousImage.h"
 
 #include <itkImage.h>
-#include <itkImageRegionIterator.h>
 #include <itkImageFileWriter.h>
+#include <itkImageRegionIterator.h>
+#include <itkImageRegionIteratorWithIndex.h>
 
 namespace iseg {
 
@@ -67,7 +68,27 @@ bool Paste(TInputImage* source, TOutputImage* destination)
 	return false;
 }
 
-template<class T> void dump_image(T *img, const std::string& file_name)
+template<class TInputImage, class TOutputImage>
+bool Paste(TInputImage* source, TOutputImage* destination, typename const TInputImage::RegionType& region)
+{
+	using OutputPixel = typename TOutputImage::PixelType;
+
+	if (source->GetBufferedRegion().IsInside(region) && destination->GetBufferedRegion().IsInside(region))
+	{
+		itk::ImageRegionIterator<TInputImage> sit(source, region);
+		itk::ImageRegionIterator<TOutputImage> dit(destination, region);
+
+		for (sit.GoToBegin(), dit.GoToBegin(); !sit.IsAtEnd() && !dit.IsAtEnd(); ++sit, ++dit)
+		{
+			dit.Set(static_cast<OutputPixel>(sit.Get()));
+		}
+		return true;
+	}
+	return false;
+}
+
+template<class T>
+void dump_image(T* img, const std::string& file_name)
 {
 #if 0
 	auto writer = itk::ImageFileWriter<T>::New();
@@ -75,6 +96,68 @@ template<class T> void dump_image(T *img, const std::string& file_name)
 	writer->SetFileName(file_name);
 	writer->Update();
 #endif
+}
+
+namespace detail {
+inline unsigned NextOddInteger(double v)
+{
+	int vint = static_cast<int>(v);
+	// make odd
+	if (vint % 2 == 0)
+	{
+		vint++;
+	}
+	// increase by 2 to next odd
+	if (v > vint)
+	{
+		vint += 2;
+	}
+	return vint;
+}
+} // namespace detail
+
+template<typename T, unsigned int Dimension>
+typename itk::Image<T, Dimension>::Pointer MakeBall(const typename itk::ImageBase<Dimension>::SpacingType& spacing, double radius)
+{
+	using image_type = typename itk::Image<T, Dimension>;
+	using iterator_type = itk::ImageRegionIteratorWithIndex<image_type>;
+
+	typename image_type::SizeType size;
+	int center[Dimension];
+	for (unsigned int d = 0; d < Dimension; ++d)
+	{
+		size[d] = detail::NextOddInteger(radius / spacing[d]);
+		center[d] = static_cast<int>(size[d] / 2);
+	}
+
+	typename image_type::RegionType region;
+	region.SetSize(size);
+
+	auto img = image_type::New();
+	img->SetRegions(region);
+	img->Allocate();
+	img->FillBuffer(0);
+	img->SetSpacing(spacing);
+
+	double radius2 = radius * radius;
+
+	iterator_type it(img, img->GetLargestPossibleRegion());
+	for (it.GoToBegin(); !it.IsAtEnd(); ++it)
+	{
+		auto idx = it.GetIndex();
+		double distance = 0;
+		for (unsigned int d = 0; d < Dimension; ++d)
+		{
+			double dx = spacing[d] * (static_cast<int>(idx[d]) - center[d]);
+			distance += dx * dx;
+		}
+
+		if (distance < radius2)
+		{
+			it.Set(1);
+		}
+	}
+	return img;
 }
 
 } // namespace iseg
