50bb3c7c3338f5ae2e4496309bf96673a5275b9b
Bryn Lloyd <lloyd@itis.ethz.ch>
2018-07-12 14:04:49 +0200

    add our header to the file

--- Thirdparty/OpenThinning/itkOpenThinning.h
+++ Thirdparty/OpenThinning/itkOpenThinning.h
@@ -1,9 +1,18 @@
+/*
+* Copyright (c) 2018 The Foundation for Research on Information Technologies in Society (IT'IS).
+*
+* This file is part of iSEG
+* (see https://github.com/ITISFoundation/osparc-iseg).
+*
+* This software is released under the MIT License.
+*  https://opensource.org/licenses/MIT
+*/
 #pragma once
 
 #include "LookupTable.h"
 
-#include <itkImageToImageFilter.h>
 #include <itkImageRegionIterator.h>
+#include <itkImageToImageFilter.h>
 
 namespace itk {
 
@@ -70,4 +79,3 @@ private:
 #ifndef ITK_MANUAL_INSTANTIATION
 #	include "itkOpenThinning.hxx"
 #endif
-
