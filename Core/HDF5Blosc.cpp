62816e08b1a7b630c12e9f3859e0ace26372bb4b
Bryn Lloyd <lloyd@itis.ethz.ch>
2018-09-14 13:20:24 +0200

    add missing precompiled header

--- Core/HDF5Blosc.cpp
+++ Core/HDF5Blosc.cpp
@@ -1,3 +1,13 @@
+/*
+* Copyright (c) 2018 The Foundation for Research on Information Technologies in Society (IT'IS).
+*
+* This file is part of iSEG
+* (see https://github.com/ITISFoundation/osparc-iseg).
+*
+* This software is released under the MIT License.
+*  https://opensource.org/licenses/MIT
+*/
+#include "Precompiled.h"
 
 #include "HDF5Blosc.h"
 
@@ -24,6 +34,5 @@ void SetBloscEnabled(bool on)
 	ISEG_WARNING("Trying to activate Blosc filter, but iSEG was not built with Blosc support.");
 #endif
 }
-
 
 } // namespace iseg
